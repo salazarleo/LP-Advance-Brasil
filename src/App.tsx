4efc94d159e7b7fdf3c790e541739c42967fd002
Leonardo Salazar <leonardomeg123@gmail.com>
2026-09-17 19:42:26 -0300

    alterações

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routers } from "./router";
 
@@ -17,6 +18,7 @@ const App = () => {
         <Sonner />
         <RouterProvider router={router} />
         <Analytics />
+        <SpeedInsights />
       </TooltipProvider>
     </QueryClientProvider>
   );
